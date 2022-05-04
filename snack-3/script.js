cfaf86f2f63db3be41d532653895a26318134eab
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-05-04 12:15:10 +0200

    aggiunco codice js esercizio 3

---
+++ snack-3/script.js
@@ -0,0 +1,18 @@
+ 
+//  Chiedo all'utente di inserire per 10 volte un numero
+ const numberone  = Number(prompt ("inserisci un numero"))
+ const numbertwo =Number(prompt ("inserisci un numero"))
+ const numberthree = Number(prompt ("inserisci un numero"))
+ const numberfor = Number(prompt ("inserisci un numero"))
+ const numberfive = Number(prompt ("inserisci un numero"))
+ const numbersix = Number(prompt ("inserisci un numero"))
+ const numberseven = Number(prompt ("inserisci un numero"))
+ const numbereight = Number(prompt ("inserisci un numero"))
+ const numbernine = Number(prompt ("inserisci un numero"))
+ const numberten = Number(prompt ("inserisci un numero"))
+
+
+// stampo la somma di tutti i numeri inseriti 
+ for (let i=1; i<=10; i++) {
+    console.log (numberone + numbertwo + numberthree + numberfor + numberfive + numbersix +  numberseven + numbereight + numbernine + numberten);
+  }
