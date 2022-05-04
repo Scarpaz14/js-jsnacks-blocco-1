236181ed99af344957f979aab7d96dea810c960d
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-05-04 15:44:32 +0200

    sistemato esercizio

--- snack-2/script.js
+++ snack-2/script.js
@@ -2,12 +2,12 @@
 const firstWord = prompt("inserisci la prima parola")
 const secondWord = prompt("inserisci la seconda parola")
 
-// se la prima parola e piu lunga stampa la prima 
+// se la prima parola e' piu lunga stampa prima la piu corta 
 if (firstWord.length > secondWord.length){
-    console.log(firstWord, secondWord);
-    //altrimenti  se la seconda parola e piu lunga stampa la seconda
-} else if (firstWord.length < secondWord.length){
     console.log(secondWord, firstWord);
+    //altrimenti  se la seconda parola e' piu lunga stampa prima la piu corta
+} else if (secondWord.length > firstWord.length){
+    console.log(firstWord, secondWord);
 }
 // altrimenti risulta pareggio 
 else{
