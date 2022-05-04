77d4541c748b800003360d7eb115f4a08df2e450
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-05-04 11:35:54 +0200

    first commit

---
+++ snack-1/script.js
@@ -0,0 +1,10 @@
+
+const numberFist = prompt("Inserisci il primo numero numero");
+const numbersecond = prompt("Inserisci il secondo numero numero");
+
+if (numberFist > numbersecond){
+    console.log( numberFist);
+}
+else{
+    console.log(numbersecond);
+}
